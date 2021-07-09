21b3bbda2c1fc6f95fc18117fdb00ca8a97ce4be
France-Guieb <77959070+France-Guieb@users.noreply.github.com>
2021-07-09 18:26:05 +1000

    homework upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# python-api-challenge
+# python-apii-challenge
---
+++ WeatherPy/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "46968aa5427fa1c6f79e3d9d66ab83b6"
+
+# Google API Key
+g_key = "AIzaSyDmNoHFsHDjCx0Nxqe6k9GbUsMKy93xdz8"
